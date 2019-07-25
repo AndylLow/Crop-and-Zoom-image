8c44b9a509b3533be2f7feb3b7698b7438f6cff6
AndylLow <39218242+AndylLow@users.noreply.github.com>
2019-07-25 09:50:44 +0300

    Proje dosyası ekle.

---
+++ Task/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Opsiyon 1",
+            "Opsiyon 2",
+            "3"});
+            this.listBox1.Location = new System.Drawing.Point(902, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 116);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 426);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(470, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(426, 426);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 457);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Task/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> imgInput;
+        Rectangle rect;
+        Point StartLocation;
+        Point EndLcation;
+        bool IsMouseDown = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.SetSelected(0,true);
+            this.MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+            //pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+
+        Bitmap originalbitmap;
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            
+            // RESIZE THE IMAGE
+             double sizeFactor = e.Delta * 0.001;
+             if (e.Delta < 0) sizeFactor = sizeFactor * -1 + 1;
+             if (e.Delta == 120) sizeFactor = 0.88;
+             if (sizeFactor == 0.88) sizeFactor = 1.12;
+             else if (sizeFactor == 1.12) sizeFactor = 0.88;
+             Bitmap originalBitmap = (Bitmap)pictureBox1.Image;
+             Size newSize = new Size((int)(originalBitmap.Width * sizeFactor), (int)(originalBitmap.Height * sizeFactor));
+             Bitmap bmp = new Bitmap(originalbitmap, newSize);
+             pictureBox1.Image = bmp;
+
+            //RESIZE RECTANGLE PLEASE
+            /*int a = Convert.ToInt32(rect.Width * sizeFactor);
+            int b = Convert.ToInt32(rect.Height * sizeFactor);
+            Size recSize = new Size(a, b);
+            rect = new Rectangle(rect.Location,recSize);
+            */
+            IsMouseDown = true;
+            pictureBox1_MouseUp_1(sender, e);
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = Environment.CurrentDirectory + "\\..\\..\\image" +"-"+ (listBox1.SelectedIndex + 1)+".jpg";
+            pictureBox1.Load();
+            originalbitmap = (Bitmap)pictureBox1.Image;
+
+            imgInput = new Image<Bgr, byte>((Bitmap)pictureBox1.Image);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDown == true)
+            {
+                EndLcation = e.Location;
+                pictureBox1.Invalidate();
+            }
+            
+
+        }
+        private Image cropImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea,
+            bmpImage.PixelFormat);
+            return (Image)(bmpCrop);
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            IsMouseDown = true;
+            StartLocation = e.Location;
+        }
+
+        private void pictureBox1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDown == true)
+            {
+                EndLcation = e.Location;
+                IsMouseDown = false;
+                if (rect != null)
+                {
+                    Image img = pictureBox1.Image;
+                    int Zoomwidth = pictureBox1.Width;
+                    int Zoomheight = pictureBox2.Height;
+
+
+                    if (img.Width <= (e.X + Zoomwidth))
+                    {
+                        Zoomwidth = img.Width - e.X;
+                    }
+                    if (img.Height <= (e.Y + Zoomheight))
+                    {
+                        Zoomheight = img.Height - e.Y;
+                    }
+
+                    Rectangle rec = new Rectangle(e.X, e.Y, Zoomwidth, Zoomheight);
+
+                    pictureBox2.Image = cropImage(img, rect);
+                }
+            }
+            /*
+            if (IsMouseDown == true)
+            {
+                EndLcation = e.Location;
+                IsMouseDown = false;
+                if (rect != null)
+                {
+                    imgInput.ROI = rect;
+                    Image<Bgr, byte> temp = imgInput.CopyBlank();
+                    imgInput.CopyTo(temp);
+                    imgInput.ROI = Rectangle.Empty;
+                    pictureBox2.Image = temp.Bitmap;
+                }
+            }*/
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (rect != null)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, GetRectangle());
+            }
+        }
+
+        private Rectangle GetRectangle()
+        {
+            rect = new Rectangle();
+            rect.X = Math.Min(StartLocation.X, EndLcation.X);
+            rect.Y = Math.Min(StartLocation.Y, EndLcation.Y);
+            rect.Width = Math.Abs(StartLocation.X - EndLcation.X);
+            rect.Height = Math.Abs(StartLocation.Y - EndLcation.Y);
+
+            return rect;
+        }
+        
+    }
+}
